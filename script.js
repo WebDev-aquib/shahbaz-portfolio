2db37f7efa4687552b48e86c6ad81126b274ce0b
Aquib <heartlesseshal891@gmail.com>
2022-01-30 13:31:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+    strings: ["Tech Mahindra", "Medusind", "Fusion", "BPO Convergence", "Teleperformance"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+})
